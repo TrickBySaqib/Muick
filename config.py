a22e9a8f40113bd5bf556ed7726d1b6ac920fc88
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-10 20:19:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -141,7 +141,7 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/219efba204a59b20c3acf.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/bd207034bc0861c593ed7.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/fd827f9a4fe8eaa3e8bf4.jpg"
