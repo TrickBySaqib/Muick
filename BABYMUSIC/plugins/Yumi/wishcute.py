6ef735399adc8ce3ccc59a80c3dc8c8e6551f8c6
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-18 02:39:17 +0530

    Update wishcute.py

--- BABYMUSIC/plugins/Yumi/wishcute.py
+++ BABYMUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from BABYMUSIC import app 
 
-SUPPORT_CHAT = "https://t.me/+C8NQDFeJJwNiNmVl"
+SUPPORT_CHAT = "/+C8NQDFeJJwNiNmVl"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
