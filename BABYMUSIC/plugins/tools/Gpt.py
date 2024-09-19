fe8bc976c4345924fb6c24d682562632cb833e91
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-19 11:59:04 +0530

    Update Gpt.py

--- BABYMUSIC/plugins/tools/Gpt.py
+++ BABYMUSIC/plugins/tools/Gpt.py
@@ -1,40 +1,31 @@
+import os
 import random
 import time
-import requests
 from BABYMUSIC import app
-from config import BOT_USERNAME
-
+import requests
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from BadAPI import api
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
-async def chat_gpt(bot, message):
-    try:
-        start_time = time.time()
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
 
+@app.on_message(
+    filters.command(
+        ["chatgpt", "ai", "ask", "gpt", "solve"],
+        prefixes=["+", ".", "/", "-", "", "$", "#", "&"],
+    )
+)
+async def chat_gpt(bot, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-                "Example:\n\n/chatgpt Where is TajMahal?"
-            )
+            "Example:**\n\n/chatgpt Where is golden temple?")
         else:
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
-
-            try:
-                # Check if "results" key is present in the JSON response
-                if "answer" in response.json():
-                    x = response.json()["answer"]
-                    end_time = time.time()
-                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
-                    await message.reply_text(
-                        f" {x}     ❍ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @UTTAM470",
-                        parse_mode=ParseMode.MARKDOWN
-                    )
-                else:
-                    await message.reply_text("No 'results' key found in the response.")
-            except KeyError:
-                # Handle any other KeyError that might occur
-                await message.reply_text("Error accessing the response.")
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n🌸 ᴘᴏᴡᴇʀᴇᴅ @lll_Oye_Zayn_lll", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
