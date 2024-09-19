15391d3b91439731e412fadd8e9d2e676b98a262
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-19 14:23:38 +0530

    Add files via upload

--- BABYMUSIC/plugins/tools/group.py
+++ BABYMUSIC/plugins/tools/group.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from BABYMUSIC import app
+from PURVIMUSIC import app
 from config import OWNER_ID
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-       await msg.reply("● ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ ●")
+       await msg.reply("ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ")
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-       await msg.reply("**● ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ ●**")
+       await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
@@ -23,7 +23,7 @@ async def brah3(app :app, message:Message):
              except Exception:
                pass
            try:
-             await message.reply(f"{text} ❍")
+             await message.reply(f"{text} 😉")
            except:
              pass
 
