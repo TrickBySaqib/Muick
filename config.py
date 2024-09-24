98c5475b75238a65fd6f25cd17771060967ee610
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-09-24 11:25:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 # Auto Broadcast Message That You Want Use In Auto Broadcast In All Groups.
 AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
 
+# Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast
+# off During Hosting, Dont Do anything here.)
+AUTO_GCAST = os.getenv("AUTO_GCAST")
+
 # ----------------------------------------------------------------
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
