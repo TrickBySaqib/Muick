94e9224da588064df26a0b76df6f0cd01b58e636
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2025-02-17 01:13:23 +0530

    Update mustjoin.py

--- BABYMUSIC/plugins/bot/mustjoin.py
+++ BABYMUSIC/plugins/bot/mustjoin.py
@@ -1,39 +1,48 @@
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from BABYMUSIC import app
 
 #--------------------------
-
-MUST_JOIN = "https://t.me/+j6FO8pK8IIkxZDU1"
+# MUST_JOIN को सही चैट यूजरनेम या आईडी से सेट करें
+MUST_JOIN = "example_chat"  # यूजरनेम के लिए (@ के बिना) या चैट आईडी (जैसे -1001234567890)
 #------------------------
+
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOIN:
+    if not MUST_JOIN:  # यदि MUST_JOIN सेट नहीं है, तो कुछ न करें
         return
+
     try:
-        try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
+        # यूजर चैट में है या नहीं, यह जांचें
+        await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+    except UserNotParticipant:
+        # यदि यूजर चैट में नहीं है, तो लिंक तैयार करें
+        if MUST_JOIN.startswith("@"):  # यदि MUST_JOIN यूजरनेम है
+            link = "https://t.me/" + MUST_JOIN[1:]  # @ को हटाकर लिंक बनाएं
+        else:  # यदि MUST_JOIN चैट आईडी है
+            try:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
-            try:
-                await msg.reply_photo(
-                    photo="https://telegra.ph/file/d24262661dda3f1832290.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
-                    reply_markup=InlineKeyboardMarkup(
+            except ChatAdminRequired:
+                print(f"मुझे {MUST_JOIN} में एडमिन एक्सेस की आवश्यकता है।")
+                return
+
+        # यूजर को जॉइन करने के लिए संदेश भेजें
+        try:
+            await msg.reply_photo(
+                photo="https://telegra.ph/file/d24262661dda3f1832290.jpg",
+                caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                reply_markup=InlineKeyboardMarkup(
+                    [
                         [
-                            [
-                                InlineKeyboardButton("๏Jᴏɪɴ๏", url=link),
-                            ]
+                            InlineKeyboardButton("๏Jᴏɪɴ๏", url=link),
                         ]
-                    )
+                    ]
                 )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
+            )
+            await msg.stop_propagation()
+        except ChatWriteForbidden:
+            pass  # यदि यूजर ने बॉट को ब्लॉक किया है, तो कुछ न करें
     except ChatAdminRequired:
-        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
+        print(f"मुझे {MUST_JOIN} में एडमिन एक्सेस की आवश्यकता है।")
